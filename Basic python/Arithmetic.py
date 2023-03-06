5354957e69201c93a36542623ff144d9d7924d9c
MinalDevpriya <73357219+MinalDevpriya@users.noreply.github.com>
2023-03-06 23:58:27 +0530

    Add files via upload

---
+++ Basic python/Arithmetic.py
@@ -0,0 +1,11 @@
+#https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    s = a + b
+    sub = a - b
+    p = a * b
+
+    print(s)
+    print(sub)
+    print(p)
